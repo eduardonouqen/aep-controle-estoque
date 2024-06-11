b1ed17db064a0654b009cfb50a94a08674aaddd3
Eduardo de França Gonzalez <eduardogonzalez@alunos.unicesumar.edu.br>
2024-06-11 11:56:19 -0300

    1.0

---
+++ App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import ToDoList from "./screens/ToDoList.js";
+import LoginScreen from "./screens/LoginScreen.js";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const getEstaLogado = async () => {
+  const credenciais = await AsyncStorage.getItem('credenciais');
+  return credenciais !== null;
+};
+
+function StackDoToDoList() {
+  const [estaLogado, setEstaLogado] = useState(null);
+
+  useEffect(() => {
+    const verificarCredenciais = async () => {
+      const logado = await getEstaLogado();
+      setEstaLogado(logado);
+    };
+
+    verificarCredenciais();
+  }, []);
+
+  if (estaLogado === null) {
+    return null;
+  }
+
+  return (
+    <Stack.Navigator initialRouteName="LoginScreen">
+      {estaLogado ? (
+        <>
+          <Stack.Screen name="ToDoList" component={ToDoList} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} options={{
+            headerShown: false
+          }} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="To Do List" component={StackDoToDoList} options={{
+          headerShown: false,
+          tabBarIcon: () => <AntDesign name="form" size={24} color="black" />
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Controle de Estoque de Alimentos para ONGs
+
+Segue a primeira versão do código utilizado para a AEP 2024.1.2 da UniCesumar
+
+    Alunos:
+    - Eduardo de França Gonzalez - RA: 22066203-2
+    - Alexandre Furuzawa Scramin - RA: 22103021-2
+    - Victor Hugo Violada Pereira - RA: 22014452-2
+
+Mais atualizações serão realizadas em breve...
